44af68168ac71c4e322f349e26cecf5bf1cc021a
Jakov <kluevja@gmail.com>
2021-04-05 18:56:26 +0400

    Queries

--- src/main/java/com/kluevja/interscore/repository/InterviewRepository.java
+++ src/main/java/com/kluevja/interscore/repository/InterviewRepository.java
@@ -3,6 +3,7 @@ package com.kluevja.interscore.repository;
 import com.kluevja.interscore.entity.Interview;
 import com.kluevja.interscore.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,10 +11,12 @@ import java.util.Set;
 
 @Repository
 public interface InterviewRepository extends JpaRepository<Interview, Long> {
+    @Query(value = "SELECT u FROM Interview u")
     List<Interview> findAll();
-    Set<Interview> findAllByDate(String date);
-    Set<Interview> findAllByInterviewerDate(User interviewer, String date);
-    Set<Interview> findAllByIntervieweeDate(User interviewee, String date);
-    Set<Interview> findAllByInterviewer(User interviewer);
-    Set<Interview> findAllByInterviewee(User interviewee);
+    @Query(value = "SELECT u FROM Interview u WHERE u.date = ?1")
+    List<Interview> findAllByDate(String date);
+    List<Interview> findAllByInterviewerDate(User interviewer, String date);
+    List<Interview> findAllByIntervieweeDate(User interviewee, String date);
+    List<Interview> findAllByInterviewer(User interviewer);
+    List<Interview> findAllByInterviewee(User interviewee);
 }
--- src/main/java/com/kluevja/interscore/service/InterviewService.java
+++ src/main/java/com/kluevja/interscore/service/InterviewService.java
@@ -1,7 +1,13 @@
 package com.kluevja.interscore.service;
 
+import com.kluevja.interscore.entity.Interview;
+import com.kluevja.interscore.repository.InterviewRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class InterviewService {
+
 }
