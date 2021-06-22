14ff45ed407def8ae98db4844ec48472782de367
KluevJakov <kluevja@gmail.com>
2021-06-22 06:39:42 +0400

    mail service

---
+++ src/main/java/com/kluevja/interscore/service/MailService.java
@@ -0,0 +1,31 @@
+package com.kluevja.interscore.service;
+
+
+import com.kluevja.interscore.entity.UserEntity;
+import com.kluevja.interscore.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MailService {
+
+    @Autowired
+    public JavaMailSender emailSender;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public void send(String mailTo, String subject, String text) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(mailTo);
+        message.setSubject(subject);
+        message.setText(text);
+        this.emailSender.send(message);
+    }
+
+    public void activate(UserEntity u) {
+        send(u.getEmail(), "Мы рады, что вы с нами", "Активируйте аккаунт по ссылке:  http://localhost:8080/activationProfile/" /*+ u.getActivationCode()*/);
+    }
+}
